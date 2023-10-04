a9d44259f776c2e4264f6d15b0522058241ed407
MHNazary <mhnazary2020@gmail.com>
2023-10-04 20:40:34 +0430

    Relation classroom with student class

--- student.rb
+++ student.rb
@@ -1,14 +1,21 @@
+require_relative 'classroom'
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
+  # constructor method for Student class that takes an age, parent_permission and name as arguments
+  def initialize(age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
