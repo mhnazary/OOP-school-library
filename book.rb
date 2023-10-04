4cfe75d986ee43b05a1be33c2d3c07c33be478e5
MHNazary <mhnazary2020@gmail.com>
2023-10-04 21:18:51 +0430

    Added Book class

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author
+
+  #   constructor method for Book class that takes a title and author as arguments
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+end
